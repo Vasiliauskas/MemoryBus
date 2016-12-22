38065aa23ea0c9aa6b1dc69e0477f0716de436b6
Vasiliauskas <vidasvasiliauskas@gmail.com>
2016-12-22 23:39:17 +0200

    test2

--- nuget/pack.ps1
+++ nuget/pack.ps1
@@ -1,5 +1,6 @@
 ﻿$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
 $assemblyFile = $root + '\bin\Build\MemoryBus.dll'
+Write-Host "$root"
 Write-Host "$assemblyFile"
 $version = [System.Reflection.Assembly]::LoadFile("$assemblyFile").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
