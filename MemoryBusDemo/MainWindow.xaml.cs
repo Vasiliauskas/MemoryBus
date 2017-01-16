2b5fd7b5ad118dfc8fcde605171f95b6d8610090
Vasiliauskas <vidasvasiliauskas@gmail.com>
2017-01-16 20:53:13 +0200

    Added demo application

---
+++ MemoryBusDemo/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using MemoryBus;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MemoryBusDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IBus _bus = new MemoryBus.MemoryBus(new DefaultConfig());
+        public MainWindow()
+        {
+            InitializeComponent();
+            _bus.Subscribe<string>(ProcessMessage);
+            _bus.SubscribeAsync<string>(s =>
+            {
+                ProcessMessage($"From async > 3: {s}");
+                return Task.FromResult(0);
+            }, s => s.Length > 3);
+
+            _bus.SubscribeAsync<string>(s =>
+            {
+                ProcessMessage($"From async: {s}");
+                return Task.FromResult(0);
+            });
+
+            _bus.RespondAsync<string, string>(s => Task.FromResult("response: " + s));
+            int a;
+            _bus.Respond<string, int>(s => int.Parse(s), s => int.TryParse(s, out a));
+            _bus.StreamRespond<string, string>((s, o) =>
+             {
+                 Task.Run(() =>
+                 {
+                     for (int i = 0; i < 5; i++)
+                     {
+                         o.OnNext(s + i.ToString());
+                         Thread.Sleep(200);
+                     }
+                     o.OnCompleted();
+                 });
+             });
+        }
+
+        private void ProcessMessage(string msg)
+            => this.Dispatcher.Invoke(() => Items.Items.Add(msg));
+
+        private void Publish(object sender, RoutedEventArgs e)
+        {
+            _bus.Publish(this.Input.Text);
+        }
+
+        private void PublishAsync(object sender, RoutedEventArgs e)
+        {
+            _bus.PublishAsync(this.Input.Text);
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            this.Items.Items.Clear();
+        }
+
+        private void Request(object sender, RoutedEventArgs e)
+        {
+            ProcessMessage($"Request: {_bus.Request<string, string>(this.Result.Text)}");
+        }
+
+        private void RequestInt(object sender, RoutedEventArgs e)
+        {
+            ProcessMessage($"Request: {_bus.Request<string, int>(this.Result.Text)}");
+        }
+
+        private async void RequestAsync(object sender, RoutedEventArgs e)
+        {
+            ProcessMessage($"Request async: {await _bus.RequestAsync<string, string>(this.Result.Text)}");
+        }
+
+        private void RequestAsyncWait(object sender, RoutedEventArgs e)
+        {
+            ProcessMessage($"Request async wait: {_bus.RequestAsync<string, string>(this.Result.Text).Result}");
+        }
+
+        private void StreamRequest(object sender, RoutedEventArgs e)
+        {
+            var msg = this.Result.Text;
+            _bus.StreamRequest<string, string>(msg, ProcessMessage, () => ProcessMessage($"Stream {msg} done"));
+            ProcessMessage($"Stream {_bus.RequestAsync<string, string>(this.Result.Text).Result}");
+        }
+    }
+}
